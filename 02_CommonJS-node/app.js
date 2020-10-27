372f972a54147cc9cfc24329562006aabbf3297c
lp8608 <lp8608@126.com>
2020-10-27 16:40:49 +0800

    修改commonjs案例

--- 02_CommonJS-node/app.js
+++ 02_CommonJS-node/app.js
@@ -6,7 +6,7 @@ let module1 = require('./module/module1')
 let module2 = require('./module/module2')
 let module3 = require('./module/module3')
 
-
+console.log(uniq(module3.arr))
 module1.foo()
 module2()
 module3.foo()
