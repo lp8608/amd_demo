603a30ecbab2967cb33a53c6c07422adb9af4572
lp8608 <lp8608@126.com>
2020-10-29 11:53:13 +0800

    add something at 20201029 by lipengak

--- 02_CommonJS-node/app.js
+++ 02_CommonJS-node/app.js
@@ -6,7 +6,7 @@ let module1 = require('./module/module1')
 let module2 = require('./module/module2')
 let module3 = require('./module/module3')
 
-
+console.log(uniq(module3.arr))
 module1.foo()
 module2()
 module3.foo()
@@ -28,3 +28,7 @@ console.log('modify by luopengc and on github web editor')
 console.log('modify by lipengak on local dev  commonjs app.js')
 
 console.log('add some thing in commonjs app.js by lipengak on local dev')
+
+console.log('add something at 20201029 by lipengak')
+
+
